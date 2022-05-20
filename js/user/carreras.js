ba1b1c0e1654a9e6b3a4409ffa98ec24b819c744
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-05-19 22:23:47 -0300

    Agregando cargos a la consulta

--- js/user/carreras.js
+++ js/user/carreras.js
@@ -6,9 +6,9 @@ $(document).ready(function () {
         console.log("Estoy entrando");
         var idCarrera = event.target.value;
         var rutaMaterias = "/selectMateriasCarrera/" + idCarrera;
+        var rutaCargos = "/selectCargosCarrera/" + idCarrera;
 
-
-
+        console.log("Estoy con los cargos");
         $.get(rutaMaterias, function (response, state) {
 
 
@@ -19,6 +19,17 @@ $(document).ready(function () {
 
             for (i = 0; i < response.length; i++) {
                 $(".materias").append("<option value='" + response[i].id + "'> [" + response[i].id + "] " + response[i].nombre + "</option>");
+            }
+        });
+
+        $.get(rutaCargos, function (response, state) {
+            if (response.length <= 0)
+                $(".cargos").append("<option selected='selected' value=''>Cargos no encontradas</option>");
+            else
+                $(".cargos").append("<option selected='selected' value=''> - Seleccione el cargo - </option>");
+
+            for (i = 0; i < response.length; i++) {
+                $(".cargos").append("<option value='" + response[i].id + "'> [" + response[i].id + "] " + response[i].nombre + "</option>");
             }
         });
     });
