203b93de2540aebc3a0ad3491a598c3838cbff26
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-05-23 12:18:54 -0300

    Buscador de usuarios

---
+++ js/listados/listado.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#buscadorProfesores").submit(function(e){
+        e.preventDefault();
+        $("#busquedaProfesor").val()
+        var url = '/usuarios/listado/profesor/'+ $("#busquedaProfesor").val();
+
+        $(location).prop('href', url)
+    })
+});
