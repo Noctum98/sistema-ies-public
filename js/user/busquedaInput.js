90e89d780c48155c0837cf0494a695c2c7d18763
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-05-23 22:28:08 -0300

    Agregando la busqueda de usuarios

--- js/user/busquedaInput.js
+++ js/user/busquedaInput.js
@@ -2,27 +2,30 @@ $(document).ready(function () {
 
     $("#username").blur(function (event) {
         $("#submit").addClass('disabled');
-        $(".spin").removeClass("d-none")
+
         $(".aviso").empty();
         var username = event.target.value;
-        if (username) {
+        if (username !=='') {
+            $(".spin").removeClass("d-none")
             var rutaBusquedaUsuario = "/buscaUsuarioByUsername/" + username;
-        }
-        $.get(rutaBusquedaUsuario, function (response, state) {
 
-            if (response.length <= 0) {
-                $("#submit").removeClass('disabled');
-                $('.card-body').removeClass('disabled')
-                $(".spin").addClass("d-none")
-                $("form :input").prop("disabled", false);
-            } else {
-                $("form :input").prop("disabled", true);
-                $('#username').prop("disabled", false);
-                $(".aviso").append("El Usuario ya existe <br/>");
-                $(".aviso").append("<a href='usuarios/detalle/" + response[0].id + "'>" + response[0].nombre + " " + response[0].apellido + "</a>")
-                $(".spin").addClass("d-none")
-            }
-        });
+            $.get(rutaBusquedaUsuario, function (response, state) {
+
+                if (response.length <= 0) {
+                    $("#submit").removeClass('disabled');
+                    $('.card-body').removeClass('disabled')
+                    $(".spin").addClass("d-none")
+                    $("form :input").prop("disabled", false);
+                    $(".aviso").append("El Usuario no existe en la base de datos ");
+                } else {
+                    $("form :input").prop("disabled", true);
+                    $('#username').prop("disabled", false);
+                    $(".aviso").append("El Usuario ya existe en la base de datos<br/>");
+                    $(".aviso").append("<a href='usuarios/detalle/" + response[0].id + "'>" + response[0].nombre + " " + response[0].apellido + "</a>")
+                    $(".spin").addClass("d-none")
+                }
+            });
+        }
 
     });
 });
