73402e962d66d13ec67eb5ae6cba52e6c3446843
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-05-27 10:03:01 -0300

    agregando alerts success and danger

--- js/user/busquedaInput.js
+++ js/user/busquedaInput.js
@@ -16,12 +16,12 @@ $(document).ready(function () {
                     $('.card-body').removeClass('disabled')
                     $(".spin").addClass("d-none")
                     $("form :input").prop("disabled", false);
-                    $(".aviso").append("El Usuario no existe en la base de datos ");
+                    $(".aviso").append("<span class='ms-5 text-right alert alert-success py-2'>Registro posible. Usuario no existente <i class='fa fa-check'></i></span>");
                 } else {
                     $("form :input").prop("disabled", true);
                     $('#username').prop("disabled", false);
-                    $(".aviso").append("El Usuario ya existe en la base de datos<br/>");
-                    $(".aviso").append("<a href='usuarios/detalle/" + response[0].id + "'>" + response[0].nombre + " " + response[0].apellido + "</a>")
+                    $(".aviso").append("<alert class='text-danger alert alert-danger py-2'> El Usuario ya existe. <i class='fa fa-times'></i> </alert>");
+                    $(".aviso").append("<a class='ml-2' href='usuarios/detalle/" + response[0].id + "'> <i class='fa fa-edit'></i>" + response[0].nombre + " " + response[0].apellido + "</a>")
                     $(".spin").addClass("d-none")
                 }
             });
