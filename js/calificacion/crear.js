f0807cd8b77b3770dc4415531a49c0acd3507765
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-06-29 13:39:28 -0300

    Parcial: recuperatorio

--- js/calificacion/crear.js
+++ js/calificacion/crear.js
@@ -5,47 +5,86 @@ $(document).ready(function () {
     $("form").submit(function (e) {
         e.preventDefault();
         var theForm = $(this);
-
         var proceso_id = theForm.attr("id");
-        var porcentaje = $('#calificacion-procentaje-' + proceso_id).val();
         var calificacion_id = $('#calificacion_id').val();
-        let url = "/procesoCalificacion";
-        let data = {
-            "porcentaje": porcentaje,
-            "proceso_id": proceso_id,
-            "calificacion_id": calificacion_id
-        }
 
-        $("#spinner-" + proceso_id).html("<div class='spinner-border text-primary mt-2' role='status' id='spinner-info'><span class='sr-only'>Loading...</span></div>")
-        $.ajax({
-            method: "POST",
-            url: url,
-            data: data,
-            //dataType: "dataType",
-            success: function (response) {
-                $("#spinner-info").remove();
 
-                if (response.errors) {
-                    for (const key in response.errors) {
-                        if (Object.hasOwnProperty.call(response.errors, key)) {
-                            const element = response.errors[key];
-                            $("#alerts").append("<div class='alert alert-danger'>" + element[0] + "</div>");
+        if(theForm.hasClass('form-recuperatorio')){
+            var porcentaje = $('#calificacion-procentaje-recuperatorio-' + proceso_id).val();
+            let url = "/procesoCalificacion/recuperatorio";
+            console.log(porcentaje);
+            let data = {
+                "porcentaje": porcentaje,
+                "proceso_id": proceso_id,
+                "calificacion_id": calificacion_id
+            }
+            $("#spinner-recuperatorio-" + proceso_id).html("<div class='spinner-border text-primary mt-2' role='status' id='spinner-rec-info'><span class='sr-only'>Loading...</span></div>")
+
+            $.ajax({
+                method: "POST",
+                url: url,
+                data: data,
+                //dataType: "dataType",
+                success: function (response) {
+                    $("#spinner-rec-info").remove();
+    
+                    if (response.errors) {
+                        for (const key in response.errors) {
+                            if (Object.hasOwnProperty.call(response.errors, key)) {
+                                const element = response.errors[key];
+                                $("#alerts").append("<div class='alert alert-danger'>" + element[0] + "</div>");
+                            }
+                        }
+                    } else {    
+                        if (response.nota_recuperatorio >= 4) {
+                            $(".nota-recuperatorio-" + proceso_id).html("<p class='text-success font-weight-bold'>" + response.nota_recuperatorio + "</p>");
+                        } else {
+                            $(".nota-recuperatorio-" + proceso_id).html("<p class='text-danger font-weight-bold'>" + response.nota_recuperatorio + "</p>");
                         }
                     }
-                } else {
-                    $("#alerts").html("");
-
-                    if (response.nota >= 4) {
-                        $(".nota-" + proceso_id).html("<p class='text-success font-weight-bold'>" + response.nota + "</p>");
-                    } else {
-                        $(".nota-" + proceso_id).html("<p class='text-danger font-weight-bold'>" + response.nota + "</p>");
-                    }
                 }
-
-
+            });
+            
+            
+        }else{
+            var porcentaje = $('#calificacion-procentaje-' + proceso_id).val();
+            let url = "/procesoCalificacion";
+            let data = {
+                "porcentaje": porcentaje,
+                "proceso_id": proceso_id,
+                "calificacion_id": calificacion_id
             }
-        });
-
+    
+            $("#spinner-" + proceso_id).html("<div class='spinner-border text-primary mt-2' role='status' id='spinner-info'><span class='sr-only'>Loading...</span></div>")
+            $.ajax({
+                method: "POST",
+                url: url,
+                data: data,
+                //dataType: "dataType",
+                success: function (response) {
+                    $("#spinner-info").remove();
+    
+                    if (response.errors) {
+                        for (const key in response.errors) {
+                            if (Object.hasOwnProperty.call(response.errors, key)) {
+                                const element = response.errors[key];
+                                $("#alerts").append("<div class='alert alert-danger'>" + element[0] + "</div>");
+                            }
+                        }
+                    } else {
+                        $("#calificacion-procentaje-recuperatorio-"+proceso_id).prop('disabled', false);
+    
+                        if (response.nota >= 4) {
+                            $(".nota-" + proceso_id).html("<p class='text-success font-weight-bold'>" + response.nota + "</p>");
+                        } else {
+                            $(".nota-" + proceso_id).html("<p class='text-danger font-weight-bold'>" + response.nota + "</p>");
+                        }
+                    }
+    
+    
+                }
+            });
+        }
     });
 
 });
