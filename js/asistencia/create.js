00f327e78b2d178bb189fdcab35ea3f010097a95
Ezequiel Lucero <ezequiel.noct@gmail.com>
2022-07-05 14:38:43 -0300

    Ajustes Asistencias

--- js/asistencia/create.js
+++ js/asistencia/create.js
@@ -2,54 +2,55 @@ $(document).ready(function () {
 
     // Asistencia Tradicional
 
-    $("form").submit(function(e) {
+    $("form").submit(function (e) {
         e.preventDefault();
         var theForm = $(this);
 
         // console.log(theForm.hasClass("form-tradicional"));
-        if(theForm.hasClass('form-tradicional'))
-        {
+        if (theForm.hasClass('form-tradicional')) {
             tradicional(theForm);
             $(this).trigger("reset");
-        }else if(theForm.hasClass('form-tradicional_7030'))
-        {
+        } else if (theForm.hasClass('form-tradicional_7030')) {
             tradicional_7030(theForm);
+        }else if(theForm.hasClass('form-modular'))
+        {
+            modular(theForm);
         }
 
-      });
 
-      var tradicional = function(form){
+    });
+
+    var tradicional = function (form) {
         var proceso_id = form.attr("id");
-        var porcentaje = $('#asis-procentaje-'+proceso_id).val();
-        let url  = "/asistencias/crear_asistencia";
+        var porcentaje = $('#asis-procentaje-' + proceso_id).val();
+        let url = "/asistencias/crear_asistencia";
         let data = {
-            "porcentaje_final":porcentaje,
-            "proceso_id":proceso_id
+            "porcentaje_final": porcentaje,
+            "proceso_id": proceso_id
         }
-        
+
         $.ajax({
             method: "POST",
             url: url,
             data: data,
             //dataType: "dataType",
             success: function (response) {
-                $(".porcentaje-"+proceso_id).html("<p>% " + porcentaje + "</p>");
-                
+                $(".porcentaje-" + proceso_id).html("<p>% " + porcentaje + "</p>");
+
             }
         });
-      }
+    }
 
-      var tradicional_7030 = function(form)
-      {
+    var tradicional_7030 = function (form) {
         var proceso_id = form.attr("id");
-        var porcentaje_presencial = $("#asis-presencial-"+proceso_id).val();
-        var porcentaje_virtual = $("#asis-virtual-"+proceso_id).val();
-        let url  = "/asistencias/crearAsistenciaSetentaTreinta";
+        var porcentaje_presencial = $("#asis-presencial-" + proceso_id).val();
+        var porcentaje_virtual = $("#asis-virtual-" + proceso_id).val();
+        let url = "/asistencias/crearAsistenciaSetentaTreinta";
 
         let data = {
-            "porcentaje_presencial":porcentaje_presencial,
+            "porcentaje_presencial": porcentaje_presencial,
             "porcentaje_virtual": porcentaje_virtual,
-            "proceso_id":proceso_id
+            "proceso_id": proceso_id
         };
 
         $.ajax({
@@ -59,12 +60,10 @@ $(document).ready(function () {
             //dataType: "dataType",
             success: function (response) {
                 console.log(response.asistencia);
-                if(response.asistencia)
-                {
-                    $(".porcentaje-"+proceso_id).html("<p>% " + response.asistencia.porcentaje_final + "</p>");
+                if (response.asistencia) {
+                    $(".porcentaje-" + proceso_id).html("<p>% " + response.asistencia.porcentaje_final + "</p>");
                     $("#alerts").html("");
-                }else
-                {
+                } else {
                     for (const key in response.errors) {
                         if (Object.hasOwnProperty.call(response.errors, key)) {
                             const element = response.errors[key];
@@ -74,7 +73,29 @@ $(document).ready(function () {
                 }
             }
         });
+    }
+
+    var modular = function (form) {
+        var proceso_id = form.attr("id");
+        var cargo_id = $('input[name=cargo_id]')[0].value;
+        var porcentaje = $('#asis-procentaje-' + proceso_id).val();
+        let url = "/asistencias/crearAsistenciaModular";
+        let data = {
+            "porcentaje": porcentaje,
+            "proceso_id": proceso_id,
+            'cargo_id': cargo_id
+        }
 
 
-      }
+        $.ajax({
+            method: "POST",
+            url: url,
+            data: data,
+            //dataType: "dataType",
+            success: function (response) {
+                $(".porcentaje-" + proceso_id).html("<p>% " + porcentaje + "</p>");
+
+            }
+        });
+    }
 });
