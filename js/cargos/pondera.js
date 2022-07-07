ccf7cf159fac1ac0a200e4fb8b65c5475e953989
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2022-07-06 23:49:21 -0300

    cambiando ponderación

---
+++ js/cargos/pondera.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $("#pondera-cargo-materia").submit(function (event) {
+        event.preventDefault();
+
+        let id = $(this).attr('id');
+        let cargo = $("input[id='cargo']",this).val();
+        let materia = $("input[id='materia']",this).val();
+        let ponderacion = $("input[id='ponderacion']",this).val();
+        $('#loader-cargo-'+cargo+'-materia-'+materia).show();
+
+        console.log(cargo + ' <br/>' + materia + ' <br/>' + ponderacion)
+
+        let url = '/ponderarCargo';
+        let data = {
+            "cargo_id": cargo,
+            "materia_id": materia,
+            "porcentaje": ponderacion,
+        };
+
+        $.ajax({
+            method: "POST",
+            url: url,
+            data: data,
+            //dataType: "dataType",
+            success: function (response) {
+
+                if (response.errors) {
+                    for (const key in response.errors) {
+                        if (Object.hasOwnProperty.call(response.errors, key)) {
+                            const element = response.errors[key];
+                            $("#alerts").append("<div class='alert alert-danger'>" + element[0] + "</div>");
+                        }
+                    }
+                }
+                $('#cargo-'+cargo+'-materia-'+materia).text(response[1])
+                $('#loader-cargo-'+cargo+'-materia-'+materia).hide();
+            },
+            done:function (){
+                $('#loader-cargo-'+cargo+'-materia-'+materia).hide();
+            }
+        });
+    });
+});
